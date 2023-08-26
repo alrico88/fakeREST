dd77ccf6671203bdd367d5df020c3189f20a41aa
Alberto Rico <alrico88@gmail.com>
2023-08-27 00:32:57 +0200

    feat: added base url env

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,3 +1,6 @@
 export default defineNitroConfig({
   alias: {},
+  runtimeConfig: {
+    baseUrl: '',
+  },
 });
--- trpc/openapi.ts
+++ trpc/openapi.ts
@@ -2,13 +2,15 @@ import { generateOpenApiDocument } from 'trpc-openapi';
 
 import { appRouter } from './router';
 
+const config = useRuntimeConfig();
+
 // Generate OpenAPI schema document
 export const openApiDocument = generateOpenApiDocument(appRouter, {
   title: 'FakeREST',
   description:
     'OpenAPI compliant REST API to generate fake data based on faker.js',
   version: '1.0.0',
-  baseUrl: 'http://localhost:3000/api',
+  baseUrl: config.baseUrl,
   docsUrl: 'https://github.com/alrico88/fakerest',
   tags: [],
 });
